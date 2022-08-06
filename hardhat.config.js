7521fefc7403f542940b9ba9e61f25bf9899bc38
Chris <rootwzrd@gmail.com>
2022-08-06 16:56:28 -0400

    commit 2 added testing and junked my Lock.js and Lock.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+//require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for(const account of accounts){
+    console.log(account.address);
+  }
+})
+
+
+// Export this object to setup my config
+// Goto https://hardhat.org/config/
+/*
+*  @type import('hardhat/config').HardhatUserConfig
+*/
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost:{}
+  },
 };
